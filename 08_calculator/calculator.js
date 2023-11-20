32d2b2d3aaf95b720f5476b42f7206dd948fdc2e
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-11-20 18:12:48 +0200

    08-Calculator: Finished "add" and "subtract".

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,13 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(digits) {
+	const total = digits.reduce(total, nextNumber)
 };
 
 const multiply = function() {
