fbc03758446413acfa2681ba37c497c360ca22fb
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-11-22 10:45:39 +0200

    08_Calculator: Finished factorial. All exercises complete and passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -21,8 +21,14 @@ const power = function(a,b) {
   return total
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0){
+    return -1
+  } else if (num === 0){
+    return 1
+  } else {
+    return (num * factorial( num -1) )
+  }
 };
 
 // Do not edit below this line
