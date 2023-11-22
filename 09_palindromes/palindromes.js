27d75a9afa82d0cd698ad145c7773e7eb50fc2f1
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-11-22 16:48:32 +0200

    09_palindrome: Finished.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const noPunc = palindrome.replace(/[^\w\s]|_/g, "")
+    const noSpace = noPunc.replace(/\s/g, '')
+    const lowStr = noSpace.toLowerCase()
 
+    const newStr = lowStr
+    const arr = newStr.split('')
+    const reverse = newStr.split('').reverse()
+
+    /* const isPalin = arr.map( (item,index) => console.log(item + ':' + index)) */
+    const isPalin = arr.every((item, index) => item === reverse[index])
+
+    return isPalin
 };
 
 // Do not edit below this line
