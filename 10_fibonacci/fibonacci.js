9ca2b32df2cebad11b082e08eebd029611872fb4
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-11-24 22:44:13 +0200

    10_fibonacci: Finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (getNum) {
+    if (getNum < 0) return "OOPS";
 
+    const n = getNum + 1;
+
+    // Create a new array of size 'n'
+    let series = new Array(n);
+
+    // Fills all places in array with 0
+    series.fill(0);
+
+    // Seed value for 1st element
+    series[0] = 0;
+
+    // Seed value for 2nd element
+    series[1] = 1;
+
+    for (let i = 2; i < n; i++) {
+
+        // Apply basic Fibonacci formulae
+        series[i] = series[i - 1] + series[i - 2];
+    }
+
+    // Print the series
+    return series[getNum];
 };
 
 // Do not edit below this line
